b59b28a469e15d02881ae7df5a4d75c317d9961b
steven2y <steven2y@hotmail.com>
2014-09-02 21:24:38 +0100

    Added simple node server to serve pages.

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+    'use strict';
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        dirs: {
+            app: 'public/app',
+            css: 'public/css',
+            vendor: 'public/vendor'
+        }
+    });
+    grunt.loadTasks('grunt');
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('unit', [
+        'karma:once'
+    ]);
+
+    grunt.registerTask('serve', [
+        'express:dev'
+    ]);
+
+};
--- public/app/main/controllers/MainController.js
+++ public/app/main/controllers/MainController.js
@@ -1,7 +1,6 @@
 define([
     'angular',
     'app/boot'
-
 ], function (angular) {
     return angular.module('myApp')
         .controller('MainController', function ($scope) {
